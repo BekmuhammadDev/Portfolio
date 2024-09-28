b34134f131e31f249730f36bb6848cfd49097e50
bekmuhammad <aripovbekmuhammad2@gmail.com>
2024-09-28 19:16:48 +0500

    recomended

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: 'class', // 'media' yoki 'class' usulini tanlashingiz mumkin
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
